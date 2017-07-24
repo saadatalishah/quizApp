a664e250cf234fc57607bffddea72f15e6e1eb6c
saadatalishah <sadatali9794@gmail.com>
2017-07-24 20:57:33 +0500

    abcd

--- signapp.js
+++ signapp.js
@@ -17,7 +17,7 @@
         // for(var i=0;i<array.length;i++)
         // {
         // }
-         var win=window.location.href='index.html';
+         var win=window.location.href='questions.html';
          console.log(win);   
          }
 
